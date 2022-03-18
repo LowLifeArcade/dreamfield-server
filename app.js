d787811c1d4250f8f6168a53de1ede3711fb9b43
Sonny Brown <lowlifearcade@gmail.com>
2022-03-17 21:55:40 -0700

    update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 
 // apply middlewares
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({ origin: '*' }));
 // app.use(express.json());
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
